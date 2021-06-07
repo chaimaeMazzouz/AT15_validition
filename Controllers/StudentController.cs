ac2c3078bf8282f331e6601cce2b68c1127507e6
chaimaeMazzouz <mazzouz.chaimae.dev@gmail.com>
2021-06-07 23:04:05 +0100

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AT15_validation.Models;
+
+namespace AT15_validation.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        List<Student> studentList = new List<Student>()
+        {
+            new Student
+            {
+                StudentId = 1,
+                StudentName = "Mazzouz",
+                Age = 21
+            },
+            new Student
+            {
+                StudentId = 2,
+                StudentName = "Alaoui",
+                Age = 21
+            }
+        };
+        public ActionResult Index()
+        {
+            return View(studentList);
+        }
+        public ActionResult Edit(int id)
+        {
+            var std = studentList.Where(s => s.StudentId == id).FirstOrDefault();
+            return View(std);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            if (ModelState.IsValid)
+            { //write code to update student
+                return RedirectToAction("Index");
+            }
+            return View(std);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AT15_validation.MvcApplication" Language="C#" %>
